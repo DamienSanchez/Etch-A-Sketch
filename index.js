af50500c62ac8725d386e2ba2823f783299d44d0
DamienSanchez <damien.sergio.sanchez@gmail.com>
2023-08-07 16:14:15 -0400

    fix: Allows for dynamic sizing in px

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('.container');
 
-let grid = 256;//total amount of divs needed to make square 
-
+let grid = 4096;//total amount of divs needed to make square 
+let num = 64
 function createGrid(gridSize){
     for(i=0; i <= gridSize; i++){
         const div = document.createElement('div');
+        div.style.width = `calc(940px / ${num})`
+        div.style.height = `calc(500px / ${num})`
         container.appendChild(div);
     }
 }
