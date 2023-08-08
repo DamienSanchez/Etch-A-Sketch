f52e8a7e9923d2f8ab0036b78d393a9f74e2a24b
DamienSanchez <damien.sergio.sanchez@gmail.com>
2023-08-08 08:04:51 -0400

    fix: the code now displays a perfect square

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const container = document.querySelector('.container');
 
-let grid = 4096;//total amount of divs needed to make square 
-let num = 64
+const userInput = parseInt(prompt("Enter how big you want the sketch pad....Max = 100: "))
+const grid = userInput*userInput
+
+//creates the grid based on user input
 function createGrid(gridSize){
     for(i=0; i <= gridSize; i++){
         const div = document.createElement('div');
-        div.style.width = `calc(940px / ${num})`
-        div.style.height = `calc(500px / ${num})`
+        div.style.width = `calc(500px / ${userInput})`
+        div.style.height = `calc(500px / ${userInput})`
         container.appendChild(div);
     }
 }
-createGrid(grid);
 
+//Hover effect that colors the "pixels"
 const divs = document.querySelector('div')
 divs.addEventListener('mouseover', function(e){
     e.target.style.backgroundColor = "coral"
 });
+
+createGrid(grid);
